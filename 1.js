0451f58c3c63968b04a45e3e99f5ee9e829bd312
Karen Shatveryan <107338482+karabass92@users.noreply.github.com>
2022-11-13 22:53:44 +0600

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,20 +1,12 @@
-//Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
-
-let testString = 'Hey fellow warriors'
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed 
+// (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 
 function spinWords(string){
-    //create temporary array of words of string
     let tempString = string.split(' ');
-
-    // by iterating every word in aray we check its length
     for (i in tempString){
-    
-    // if length more than 4, we split it in chars and reverse, then join
         if (tempString[i].length > 4){
             tempString[i] = tempString[i].split('').reverse().join('');
         }
     }
     return tempString.join(" ");
 }
-
-console.log(spinWords(testString));
